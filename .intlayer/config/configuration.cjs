244b0e9fba9d3d678aca69af5ee5a3c4fe6d6371
Kethmika2004 <kethmikakadyasandu@gmail.com>
2026-06-15 15:42:06 +0530

    initialize Intlayer internationalization configuration and directory structure

--- .intlayer/config/configuration.cjs
+++ .intlayer/config/configuration.cjs
@@ -91,7 +91,7 @@ const content = {
     ".content.yml"
   ],
   "contentDir": [
-    "C:\\Users\\ssn computres\\Desktop\\chrono_med\\Chrono_Med_2.0\\src"
+    "C:\\Users\\ssn computres\\Desktop\\chrono_med\\Chrono_Med_2.0"
   ],
   "codeDir": [
     "C:\\Users\\ssn computres\\Desktop\\chrono_med\\Chrono_Med_2.0"
